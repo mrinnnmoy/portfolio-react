ba3dd351390e7442f906ece925f2e3edbe5b1806
mrinnnmoy <mrinmoy202000@gmail.com>
2022-10-15 19:33:00 +0530

    build navbar-section

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="n-wrapper">
+            <div className="n-left">
+                <div className="n-name">Mrinmoy</div>
+                <span>Toggle</span>
+            </div>
+            <div className="n-right">
+                <div className="n-list">
+                    <ul>
+                        <li>Home</li>
+                        <li>Services</li>
+                        <li>Experience</li>
+                        <li>Portfolio</li>
+                        <li>Testimonials</li>
+                    </ul>
+                </div>
+                <button className="button">
+                    Contact US
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
