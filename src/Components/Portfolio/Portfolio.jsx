39c04b164a72c093297f7b23878c1275069446e7
mrinnnmoy <mrinmoy202000@gmail.com>
2022-10-19 19:27:24 +0530

    build style-portfolio

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -4,33 +4,37 @@ import Blog from "../../img/blog.png";
 import Todo from "../../img/todo.png";
 import Health from "../../img/healthfix.png";
 import Urban from "../../img/urbanash.png";
-import {Swiper, SwiperSlide} from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 
 const Portfolio = () => {
-  return (
-    <div className="portfolio">
-        {/* Header */}
-        <span>Recent Projects</span>
-        <span>Portfolio</span>
+    return (
+        <div className="portfolio">
+            {/* Header */}
+            <div className="heading">
+                <span>Portfolio</span>
+                <span>Recent Projects</span>
+            </div>
 
-        {/* Slider */}
-        <Swiper className="portfolio-slider">
-            <SwiperSlide>
-                <img src={Blog} alt="Blog" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Health} alt="Health" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Urban} alt="UrbanAsh" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Todo} alt="Todo" />
-            </SwiperSlide>
-        </Swiper>
-    </div>
-  );
+
+            {/* Slider */}
+            <Swiper className="portfolio-slider">
+                <SwiperSlide>
+                    <img src={Blog} alt="Blog" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Health} alt="Health" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Urban} alt="UrbanAsh" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Todo} alt="Todo" />
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
 };
 
 export default Portfolio;
