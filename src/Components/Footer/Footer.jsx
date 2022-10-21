883e3bc16cd6960b29789df1df6f85920e29a73e
mrinnnmoy <mrinmoy202000@gmail.com>
2022-10-21 07:52:55 +0530

    build footer-section

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Footer.css";
+import Wave from "../../img/wave.png";
+import Linkedin from "@iconscout/react-unicons/icons/uil-linkedin";
+import Twitter from "@iconscout/react-unicons/icons/uil-twitter";
+import Github from "@iconscout/react-unicons/icons/uil-github";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <img src={Wave} style={{width: "100%"}} alt="wave-img" />
+      <div className="f-content">
+        <span>mrinmoy202000@gmail.com</span>
+        <div className="f-icons">
+            <Linkedin color="white" size={"3rem"} />
+            <Twitter color="white" size={"3rem"} />
+            <Github color="white" size={"3rem"} />
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Footer;
