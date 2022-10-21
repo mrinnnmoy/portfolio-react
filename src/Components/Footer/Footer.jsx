5b569b3e4dfbd8231f725f40bff9dd36058955de
mrinnnmoy <mrinmoy202000@gmail.com>
2022-10-21 08:01:57 +0530

    build style-footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,19 +6,21 @@ import Twitter from "@iconscout/react-unicons/icons/uil-twitter";
 import Github from "@iconscout/react-unicons/icons/uil-github";
 
 const Footer = () => {
-  return (
-    <div className="footer">
-      <img src={Wave} style={{width: "100%"}} alt="wave-img" />
-      <div className="f-content">
-        <span>mrinmoy202000@gmail.com</span>
-        <div className="f-icons">
-            <Linkedin color="white" size={"3rem"} />
-            <Twitter color="white" size={"3rem"} />
-            <Github color="white" size={"3rem"} />
+    return (
+        <div className="footer">
+            <img src={Wave} style={{ width: "100%" }} alt="wave-img" />
+            <div className="f-content">
+                <span class="footer__copy">
+                    &#169; Mrinmoy Porel. All rights reserved.
+                </span>
+                <div className="f-icons">
+                    <Linkedin color="white" size={"3rem"} />
+                    <Twitter color="white" size={"3rem"} />
+                    <Github color="white" size={"3rem"} />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 };
 
 export default Footer;
