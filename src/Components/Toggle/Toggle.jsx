0b0bbe49d7c2a562baee75ea6195a9034b554a71
mrinnnmoy <mrinmoy202000@gmail.com>
2022-10-22 20:26:43 +0530

    build toggle-button-function

--- src/Components/Toggle/Toggle.jsx
+++ src/Components/Toggle/Toggle.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import "./Toggle.css";
 import Moon from "@iconscout/react-unicons/icons/uil-moon";
 import Sun from "@iconscout/react-unicons/icons/uil-sun";
+import { themeContext } from "../../Context";
+import { useContext } from 'react';
 
 const Toggle = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  const handleClick = () => {
+    theme.dispatch({ type: "toggle" });
+  };
   return (
-    <div className="toggle">
-        <Moon />
-        <Sun />
-      
-      <div className="t-button" style={{left: "2px", right: "2px"}}></div>
+    <div className="toggle" onClick={handleClick}>
+      <Moon />
+      <Sun />
+
+      <div className="t-button"
+        style={darkMode ? { left: "2px" } : { right: "2px" }}>
+      </div>
     </div>
   );
 };
